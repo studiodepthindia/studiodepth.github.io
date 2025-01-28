cb42fe8ed0404da85921957f8bd52c6b18039b69
studiodepthindia <studiode.pthindia@gmail.com>
2025-01-28 19:30:33 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    if (name && email && message) {
+        alert(`Thank you for contacting StudioDepth, ${name}! We will get back to you soon.`);
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
